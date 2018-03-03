a908a7ed44e0ace3f3f4f9e85ad9f48ecff31c66
Agung <agung@192.168.2.13>
2018-03-03 21:44:19 +0100

    V1_bijnaklaar
    Nog smooth scroll, en eventueel andere events fixen,
    
    Responsive en dergelijke al af

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -7,44 +7,65 @@ var fifthSection = document.querySelector('ul li:nth-of-type(5) a div.circle');
 var sixthSection = document.querySelector('ul li:nth-of-type(6) a div.circle');
 
 function countHeight(){
-  counter.innerHTML="scrollpos = " + window.pageYOffset + " en heightWindow = " + window.outerHeight;
+  counter.innerHTML="scrollpos = " + window.pageYOffset + " en heightWindow = " + window.innerHeight;
 }
 
 window.addEventListener('scroll', countHeight);
 
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+var smoothContainer = document.querySelector('.scrolling-box');
+
 function changeColor(){
   var height = window.pageYOffset;
-  if(height < 600 ){
+
+  if(height < h/2 ){
     firstSection.classList.add('white');
     secondSection.classList.remove('white');
 
-  }if(height > 600){
+  }if(height > h/2){
     firstSection.classList.remove('white');
     secondSection.classList.add('white');
     thirdSection.classList.remove('white');
   }
-  if(height > 1200){
+  if(height >= h+(h/2)){
     secondSection.classList.remove('white');
     thirdSection.classList.add('white');
     fourthSection.classList.remove('white');
   }
-  if(height > 1850){
+  if(height >= h*2+(h/2)){
     thirdSection.classList.remove('white');
     fourthSection.classList.add('white');
     fifthSection.classList.remove('white');
   }
-  if(height > 2500){
+  if(height >= h*3+(h/2)){
     fourthSection.classList.remove('white');
     fifthSection.classList.add('white');
     sixthSection.classList.remove('white');
   }
-  if(height > 3100){
+  if(height >= h*4+(h/2)){
     fifthSection.classList.remove('white');
     sixthSection.classList.add('white');
   }
+
 
 }
 
 window.addEventListener('scroll', changeColor);
 
 console.log(window.scrollY);
+
+// proberen smooth scroll
+
+// var secondLi = document.querySelector('ul li:nth-of-type(2) a');
+//
+// function smoothScroll(){
+//   window.scroll({
+//   top: window.innerHeight,
+//   left: 0,
+//   behavior: 'smooth'
+// });
+// }
+//
+// secondLi.addEventListener('click', smoothScroll,false);
