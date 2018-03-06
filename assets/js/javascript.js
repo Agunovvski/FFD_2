ecbccdc61955285bcfc63e38ad566f0a74744ed2
Agung <agung@046.189.028.145.hva.nl>
2018-03-06 17:41:25 +0100

    V3_smoothscroll
    Smoothscroll added, alleen nog puntjes op de i

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -15,7 +15,9 @@ var sixthSection = document.querySelector('ul li:nth-of-type(6) a div.circle');
 var w = window.innerWidth;
 var h = window.innerHeight;
 
-var smoothContainer = document.querySelector('.scrolling-box');
+// var smoothContainer = document.querySelector('.scrolling-box');
+
+var progressMenu = document.querySelector('div.progress-menu');
 
 function changeColor(){
   var height = window.pageYOffset;
@@ -52,29 +54,78 @@ function changeColor(){
 
 }
 
-window.addEventListener('scroll', changeColor);
 
 
 
-var progressMenu = document.querySelector('.progress-menu');
+var progressMenu = document.querySelector('div.progress-menu');
+var pInner = document.querySelector('p.progress-count');
+var progressSlash = document.querySelectorAll('.progress-slash');
 
 function progressProject(){
 
   var height = window.pageYOffset;
+  if (height <= h+(h/2)) {
+    // progressMenu.classList.remove('pmshow');
+    // pInner.innerHTML = "1";
+  }
   if(height >= h+(h/2)){
-    progressMenu.classList.add('pmshow');
+    // progressMenu.classList.add('pmshow');
+    progressSlash[0].classList.add('slash-show');
   }
   if(height >= h*2+(h/2)){
-
+    pInner.innerHTML = "2";
+    progressSlash[1].classList.add('slash-show');
   }
   if(height >= h*3+(h/2)){
-
+    pInner.innerHTML = "3";
+    progressSlash[2].classList.add('slash-show');
   }
   if(height >= h*4+(h/2)){
-
+    pInner.innerHTML = "4";
+    progressSlash[3].classList.add('slash-show');
   }
 }
 
+var portAbout = document.querySelector('section:nth-of-type(2)');
+var portOne = document.querySelector('section:nth-of-type(3)');
+var portTwo = document.querySelector('section:nth-of-type(4)');
+var portThree = document.querySelector('section:nth-of-type(5)');
+var portFour = document.querySelector('section:nth-of-type(6)');
+
+function showSection(){
+
+  var height = window.pageYOffset;
+  if (height <= h+(h/2)) {
+  }
+  if(height >= h+(h/2)){
+    portOne.classList.add('section-show');
+  }
+  if(height >= h*2+(h/2)){
+    portTwo.classList.add('section-show');
+  }
+  if(height >= h*3+(h/2)){
+    portThree.classList.add('section-show');
+  }
+  if(height >= h*4+(h/2)){
+    portFour.classList.add('section-show');
+  }
+}
+
+var toWork = document.querySelector('.to-work');
+var toAbout = document.querySelector('.to-about');
+
+function scrollTowork(){
+  portOne.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToabout(){
+  portAbout.scrollIntoView({ behavior: 'smooth' });
+}
+
+toAbout.addEventListener('click', scrollToabout);
+toWork.addEventListener('click', scrollTowork);
+window.addEventListener('scroll', showSection);
+window.addEventListener('scroll', changeColor);
 window.addEventListener('scroll', progressProject);
 
 // proberen smooth scroll
@@ -92,11 +143,11 @@ window.addEventListener('scroll', progressProject);
 // secondLi.addEventListener('click', smoothScroll,false);
 
 
-var workButton = document.querySelector('.project .button');
-var body = document.body;
-
-function scrollWork(){
-  body.scrollTop = h+(h/2);
-}
-
-workButton.addEventListener('scroll', scrollWork, false);
+// var workButton = document.querySelector('.project .button');
+// var body = document.body;
+//
+// function scrollWork(){
+//   body.scrollTop = h+(h/2);
+// }
+//
+// workButton.addEventListener('scroll', scrollWork, false);
